8dccd9d9547ca052ca953dd620d839a93c691368
Biscuit <84092899+BiscuiTheHobkin@users.noreply.github.com>
2024-04-11 15:42:47 +0200

    Code upload

---
+++ AstralClone/AstralClone.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using MelonLoader;
+using Il2CppVRC.Core;
+
+namespace Astrum
+{
+	public class AstralClone : MelonMod
+	{
+		public override void OnApplicationStart()
+		{
+			MelonLogger.Msg("Patching AllowAvatarCopying...");
+			base.HarmonyInstance.Patch(typeof(APIUser).GetProperty("allowAvatarCopying").GetSetMethod(), new HarmonyMethod(typeof(AstralClone).GetMethod("Hook", BindingFlags.Static | BindingFlags.NonPublic)), null, null, null, null);
+            MelonLogger.Msg("AllowAvatarCopying Patched !");
+        }
+
+		private static void Hook(ref bool __0)
+		{
+			__0 = true;
+		}
+	}
+}
