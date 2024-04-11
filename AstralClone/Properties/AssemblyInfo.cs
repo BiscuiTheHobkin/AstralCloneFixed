e8bc8f895dc3d3f430fdc64c93ca3d0689c13250
Biscuit <84092899+BiscuiTheHobkin@users.noreply.github.com>
2024-04-11 15:46:49 +0200

    Code Upload

--- AstralClone/Properties/AssemblyInfo.cs
+++ AstralClone/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using Astrum;
 using MelonLoader;
 
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: MelonInfo(typeof(AstralClone), "AstralCloneFixed", "0.1.0", null, "github.com/Astrum-Project/AstralClone")]
+[assembly: MelonInfo(typeof(AstralClone), "AstralCloneFixed", "0.1.0", null, "Made by Astrum-Project fixed by biscuit")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonColor(ConsoleColor.DarkMagenta)]
